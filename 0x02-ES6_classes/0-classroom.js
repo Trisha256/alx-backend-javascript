8662f1cb0e854290f3abe462c28bc4163ce88c43
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-08-09 06:35:12 +0400

    0-classroom.js update

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,7 @@
-export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
+export default class ClassRoom
+{
+	constructor(maxStudentsSize)
+	{
+		this._maxStudentsSize = maxStudentsSize;
+	}
 }
