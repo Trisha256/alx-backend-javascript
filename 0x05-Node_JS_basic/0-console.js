e2ae2e22dc902a40f83f68e2d14adb2727a1e98c
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-10-01 19:35:10 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * logs a message to the console
+ * @param {string) message - message to log
+ * @ return {void}
+ */
+//Arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * exports the displayMessage function
+ * @module: displayMessage
+ * @function
+ */
+//default module export
+module.exports = displayMessage;
